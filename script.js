283d0d650195c63bc998fd91a956720235231229
Bala Chandra Akula <115984987+BalaChandraAkula@users.noreply.github.com>
2023-02-16 14:34:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your code here
+let ans = 0;
+const btn = document.querySelector(".submitInput");
+
+//function
+
+function showResult(event) {
+  const values = document.querySelector(".textInput").value;
+  const temp = values.split(",");
+  let arr = [];
+  for (let i = 0; i < temp.length; i++) {
+    arr.push(Number(temp[i]));
+  }
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+  while (arr.length > 1) {
+    let first = arr.shift();
+    let second = arr.shift();
+    ans += first + second;
+    arr.push(first + second);
+    arr.sort(function (a, b) {
+      return a - b;
+    });
+  }
+  document.querySelector("#result").textContent = ans;
+  console.log(ans);
+}
+
+btn.addEventListener("click", showResult);
